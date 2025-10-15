0015231c72f71dd2c42e54ad32956d927004ec6f
sylenthwave <sylenthwave@gmail.com>
2025-10-15 18:42:33 +0800

    Initial Commit

---
+++ Lumen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lumen
+//
+//  Created by sylenthwave on 2025/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ LumenTests/LumenTests.swift
@@ -0,0 +1,17 @@
+//
+//  LumenTests.swift
+//  LumenTests
+//
+//  Created by sylenthwave on 2025/10/15.
+//
+
+import Testing
+@testable import Lumen
+
+struct LumenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
