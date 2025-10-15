c45ad86ce4281ee8fa0365b5407e9e84c2b5bd82
sylenthwave <sylenthwave@gmail.com>
2025-10-15 18:47:03 +0800

    feat: 初始化配置

--- Lumen/Modules/Root/RootViewController.swift
+++ Lumen/Modules/Root/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  Lumen
 //
 //  Created by sylenthwave on 2025/10/15.
@@ -7,11 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
 
 
